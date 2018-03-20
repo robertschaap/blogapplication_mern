e3f32a6eed4835dd636f03fc43594f4dd00d8921
robertschaap <robert.schaap@mac.com>
2018-03-20 20:27:15 +0100

    Further cleanup on setup, redundancies removed

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,9 +1,4 @@
-'use strict';
-
 const path = require('path');
-
-// This is a custom Jest transformer turning file imports into filenames.
-// http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
   process(src, filename) {
