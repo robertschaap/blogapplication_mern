f0ab5877f1c7807e16e0b2dff001572c92c97e3f
robertschaap <robert.schaap@mac.com>
2018-05-11 23:30:50 +0200

    Moved posts to controllers

--- app.js
+++ app.js
@@ -4,47 +4,4 @@ const myport = process.env.PORT || 3000;
 
 app.listen(myport, () => console.log(`Now listening on port ${myport}`));
 
-app.get("/api/posts/" , (req, res) => {
-  const posts =  [
-    { id: 1, title: "Title", author: "Author" },
-    { id: 2, title: "Title", author: "Author" },
-    { id: 3, title: "Title", author: "Author" },
-    { id: 4, title: "Title", author: "Author" },
-    { id: 5, title: "Title", author: "Author" },
-    { id: 6, title: "Title", author: "Author" }
-  ];
-
-  res.json(posts);
-});
-
-app.get("/api/posts/:category(\\D+)" , (req, res) => {
-  const posts =  [
-    { id: 1, title: "Title", author: "Author" },
-    { id: 2, title: "Title", author: "Author" },
-    { id: 3, title: "Title", author: "Author" },
-    { id: 4, title: "Title", author: "Author" },
-    { id: 5, title: "Title", author: "Author" },
-    { id: 6, title: "Title", author: "Author" }
-  ];
-
-  res.json(posts);
-});
-
-app.get("/api/posts/:id(\\d+)" , (req, res) => {
-  const post = {
-    post: {
-      postAuthor: {
-        firstName: "Robert",
-        lastName: "Schaap",
-        bio: "Lalala"
-      },
-      postBody: {
-        title: "A Post",
-        body: "Lorum ipsum dolor sit amet"
-      }
-    },
-    comments: []
-  };
-
-  res.json(post);
-});
+app.use(require("./controllers"));
---
+++ controllers/posts.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/" , (req, res) => {
+  const posts =  [
+    { id: 1, title: "Title", author: "Author" },
+    { id: 2, title: "Title", author: "Author" },
+    { id: 3, title: "Title", author: "Author" },
+    { id: 4, title: "Title", author: "Author" },
+    { id: 5, title: "Title", author: "Author" },
+    { id: 6, title: "Title", author: "Author" }
+  ];
+
+  res.json(posts);
+});
+
+router.get("/:category(\\D+)" , (req, res) => {
+  const posts =  [
+    { id: 1, title: "Title", author: "Author" },
+    { id: 2, title: "Title", author: "Author" },
+    { id: 3, title: "Title", author: "Author" },
+    { id: 4, title: "Title", author: "Author" },
+    { id: 5, title: "Title", author: "Author" },
+    { id: 6, title: "Title", author: "Author" }
+  ];
+
+  res.json(posts);
+});
+
+router.get("/:id(\\d+)" , (req, res) => {
+  const post = {
+    post: {
+      postAuthor: {
+        firstName: "Robert",
+        lastName: "Schaap",
+        bio: "Lalala"
+      },
+      postBody: {
+        title: "A Post",
+        body: "Lorum ipsum dolor sit amet"
+      }
+    },
+    comments: []
+  };
+
+  res.json(post);
+});
+
+module.exports = router;
