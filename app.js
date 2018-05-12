071385790c79e58271d407d2d5bd5023d3e32532
robertschaap <robert.schaap@mac.com>
2018-05-12 17:37:00 +0200

    Database init/drop/populate functionality

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const myport = process.env.PORT || 3000;
+const dbport = process.env.MONGOPORT || 27017;
 
 app.listen(myport, () => console.log(`Now listening on port ${myport}`));
 
 app.use(require("./controllers"));
+
+const models = require("./models");
+const argv = process.argv[2];
+
+argv ? models.run(dbport, argv) : models.run(dbport);
--- models/index.js
+++ models/index.js
@@ -1,12 +1,45 @@
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb:/localhost:27017/blogapplication");
-mongoose.connection.on("error", console.error.bind(console), "MDB Connect Err");
-mongoose.Promise = global.Promise;
+
+function run(dbPort, cliInput) {
+  switch (cliInput) {
+  case "populateDB":
+    populateDB();
+    break;
+
+  case "dropDB":
+    dropDB();
+    break;
+
+  default:
+    initialiseDB(dbPort);
+    break;
+  }
+}
+
+function initialiseDB(dbPort) {
+  mongoose.connect(`mongodb://localhost:${dbPort}/blogapplication`);
+  mongoose.connection.on("error", console.error.bind(console), "MDB Connect Err");
+  mongoose.Promise = global.Promise;
+}
+
+function dropDB() {
+  console.log("dropping database");
+  mongoose.connection.dropDatabase();
+  process.exit();
+}
+
+function populateDB() {
+  Populate();
+  process.exit();
+}
 
 const Comments = require("./comments");
 const Posts = require("./posts");
 const Users = require("./users");
+const Populate = require("./populate");
 
 exports.Comment = Comments;
 exports.Posts = Posts;
 exports.Users = Users;
+exports.run = run;
