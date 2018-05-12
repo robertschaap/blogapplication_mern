3120ebebd0c42d1083633b2dd793c554530caab2
robertschaap <robert.schaap@mac.com>
2018-05-12 17:33:41 +0200

    Users static methods

--- models/users.js
+++ models/users.js
@@ -8,11 +8,25 @@ const UserSchema = Schema({
   firstName: String,
   lastName: String,
   email: String,
+  userName: String,
   bio: String,
   password: String
 }, {
   timestamps: true
 });
+
+UserSchema.statics.new = (user) => {
+  let record = new Users({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    userName: user.userName,
+    bio: user.bio,
+    password: user.password
+  });
+
+  record.save(err => err ? err : console.log("done"));
+};
 
 const Users = mongoose.model("User", UserSchema);
 module.exports = Users;
