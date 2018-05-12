9ff007d7bb956565e40d481164d03fe8b7d805fc
robertschaap <robert.schaap@mac.com>
2018-05-12 17:32:46 +0200

    Posts static methods

--- models/posts.js
+++ models/posts.js
@@ -12,5 +12,27 @@ const PostSchema = Schema({
   timestamps: true
 });
 
+// allPosts all posts with data of who posted it
+PostSchema.statics.allPosts = () => {
+  Posts.find();
+};
+
+// onePost one post with user data and comments
+PostSchema.statics.onePost = () => {
+  Posts.find();
+};
+
+// createPost
+PostSchema.statics.new = (post) => {
+  let record = new Posts({
+    title: post.title,
+    body: post.body,
+    category: post.category,
+    userId: post.userId
+  });
+
+  record.save(err => err ? err : console.log("done"));
+};
+
 const Posts = mongoose.model("Post", PostSchema);
 module.exports = Posts;
