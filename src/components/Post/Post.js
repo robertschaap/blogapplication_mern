d033abe14bfc3524701fd5caa17fc53d7f9ed2fe
robertschaap <robert.schaap@mac.com>
2018-06-16 19:20:48 +0200

    Post: replaced array return with fragment

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styles from "./Post.scss";
 import Comments from "../Comments";
 import CommentForm from "../CommentForm";
@@ -7,28 +7,30 @@ const Post = ({ post, comments }) => {
   const { postAuthor, postBody } = post;
 
   return (
-    <section className={styles.background} key={0} />,
-    <section className={styles.component} key={1}>
-      <div>
-        <article>
-          <div className={styles.postHeader}>
-            <div className={styles.postTop}>
-              <h2>{postBody.title}</h2>
-              <p>{postAuthor.firstName} {postAuthor.lastName}</p>
-              <p>{postAuthor.bio}</p>
+    <Fragment>
+      <section className={styles.background} />
+      <section className={styles.component}>
+        <div>
+          <article>
+            <div className={styles.postHeader}>
+              <div className={styles.postTop}>
+                <h2>{postBody.title}</h2>
+                <p>{postAuthor.firstName} {postAuthor.lastName}</p>
+                <p>{postAuthor.bio}</p>
+              </div>
+              <div className={styles.postBottom}>
+                <img />
+              </div>
             </div>
-            <div className={styles.postBottom}>
-              <img />
-            </div>
-          </div>
-          <p>{postBody.body}</p>
-        </article>
+            <p>{postBody.body}</p>
+          </article>
 
-        <Comments comments={comments} />
-        <CommentForm />
+          <Comments comments={comments} />
+          <CommentForm />
 
-      </div>
-    </section>
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
