0c7c11d93b6bf491359c9ac04b6f48e8a28f00ef
robertschaap <robert.schaap@mac.com>
2018-06-17 08:12:24 +0200

    UsersNew: container setup

---
+++ src/containers/UsersNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SignupForm from "../components/SignupForm";
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <main>
+        <SignupForm />
+      </main>
+    );
+  }
+}
+
+export default PostsNew;
