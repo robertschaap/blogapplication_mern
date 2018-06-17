a6ab08f4fead89e3bfb3a634b66c036c7883bc9f
robertschaap <robert.schaap@mac.com>
2018-06-17 08:12:13 +0200

    PostsNew: container setup

---
+++ src/containers/PostsNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PostForm from "../components/PostForm";
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <main>
+        <PostForm />
+      </main>
+    );
+  }
+}
+
+export default PostsNew;
