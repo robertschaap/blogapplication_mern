035e5518bcfdd73a50a072d111b50dff1a7e6a4e
robertschaap <robert.schaap@mac.com>
2018-06-17 08:09:45 +0200

    Login: added buttons for user options

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,8 @@ import styles from "./Login.scss";
 
 const Login = () => (
   <div className={styles.component}>
+    <a href="#">+</a>
+    <a href="#">Log Out</a>
     <a href="#">Sign In</a>
     <a href="#">Sign Up</a>
   </div>
