dfd714abff2887ea820d6398035f5a9ef97db602
robertschaap <robert.schaap@mac.com>
2018-07-03 22:20:22 +0200

    Refactor on postform

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,26 +1,46 @@
 import React from "react";
 import styles from "./PostForm.scss";
 
-const PostForm = () => (
-  <section className={styles.component}>
-    <h2>Create Something new</h2>
-    <form action="">
-      <label>Title</label>
-      <input type="text" name="title" />
-      <label>Body</label>
-      <input type="text" name="body" />
-      <label>Image (JPEG Only)</label>
-      <input type="file" accept=".jpg" name="banner" />
-      <label>Category</label>
-      <select name="category">
-        <option value="Technology">Technology</option>
-        <option value="Design">Design</option>
-        <option value="Entrepreneur">Entrepreneur</option>
-        <option value="Anything">Anything</option>
-      </select>
-      <button type="submit">Post</button>
-    </form>
-  </section>
-);
+const PostForm = ({ value, onChange, onSubmit }) => {
+
+  const { title, body, category } = value;
+
+  return (
+    <section className={styles.component}>
+      <h2>Create Something New</h2>
+      <form onSubmit={onSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={onChange} />
+
+        <label>Body</label>
+        <textarea
+          type="text"
+          name="body"
+          value={body}
+          onChange={onChange} />
+
+        <label>Image (JPEG Only)</label>
+        <input
+          type="file"
+          accept=".jpg"
+          name="banner" />
+
+        <label>Category</label>
+        <select name="category" value={category} onChange={onChange}>
+          <option value="Technology">Technology</option>
+          <option value="Design">Design</option>
+          <option value="Entrepreneur">Entrepreneur</option>
+          <option value="Anything">Anything</option>
+        </select>
+
+        <button type="submit">Post</button>
+      </form>
+    </section>
+  );
+};
 
 export default PostForm;
--- src/containers/PostsNew.js
+++ src/containers/PostsNew.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import PostForm from "../components/PostForm";
 
 class PostsNew extends Component {
+  state = {
+    title: "",
+    body: "",
+  }
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  }
+
   render() {
     return (
       <main>
-        <PostForm />
+        <PostForm value={this.state} onChange={this.handleChange} onSubmit={this.handleSubmit} />
       </main>
     );
   }
