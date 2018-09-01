3b964978ed8c7fb523b878a82c1cf3a3de58ea6c
robertschaap <robert.schaap@mac.com>
2018-09-01 19:58:55 +0200

    Extracted UserMenu to own component

--- src/components/LoginMenu/LoginMenu.js
+++ src/components/LoginMenu/LoginMenu.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 
 const LoginMenu = () => (
   <div className={styles.component}>
-    <Link to="/posts/new/">+</Link>
-    <Link to="/users/logout/">Log Out</Link>
     <Link to="/users/login/">Sign In</Link>
     <Link to="/users/new/">Sign Up</Link>
   </div>
---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./UserMenu.scss";
+import { Link } from "react-router-dom";
+
+const UserMenu = () => (
+  <div className={styles.component}>
+    <Link to="/posts/new/">+</Link>
+    <Link to="/users/logout/">Log Out</Link>
+  </div>
+);
+
+export default UserMenu;
