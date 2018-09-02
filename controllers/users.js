2fa3b88fc7890100c7b642f6520ef305dff9f1d6
robertschaap <robert.schaap@mac.com>
2018-09-02 20:07:04 +0200

    users controller: setup basic jwt logic

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const db = require("../models");
 
 router.post("/new" , (req, res) => {
   console.log(req.body);
   res.json("done");
 });
 
-router.get("/login", (req, res) => {
-  console.log(req.body);
-  res.json("done");
+// TODO: proper API status codes and pw enc/decr
+router.post("/login", (req, res) => {
+  const { userName, password } = req.body;
+
+  db.Users.findOne({ userName })
+    .then(user => {
+      if (user && user.password === password) {
+        res.json({
+          token: jwt.sign({ id: user._id }, "thesecret", { expiresIn: 86400 })
+        });
+      } else {
+        res.json("denied");
+      }
+    });
 });
 
 router.get("/logout", (req, res) => {
