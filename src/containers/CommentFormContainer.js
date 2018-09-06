f266262bd1283b6dc825885132328c35b09e3bcf
robertschaap <robert.schaap@mac.com>
2018-09-06 22:41:38 +0200

    CommentFormContainer: added form handling logic

--- src/containers/CommentFormContainer.js
+++ src/containers/CommentFormContainer.js
@@ -2,9 +2,44 @@ import React, { Component } from "react";
 import CommentForm from "../components/CommentForm";
 
 class CommentFormContainer extends Component {
+  state = {
+    formData: {
+      comment: ""
+    }
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      formData: {
+        comment: value
+      }
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("/api/comments/new", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state.formData)
+    })
+    .then(res => {
+      // TODO: handle reponse
+      console.log(res);
+    });
+  }
+
   render() {
     return (
-      <CommentForm />
+      <CommentForm
+        value={this.state.formData}
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit} />
     );
   }
 }
