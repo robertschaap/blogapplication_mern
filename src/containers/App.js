b36ef5207be8617a2d29bea0785347f259fa5f39
robertschaap <robert.schaap@mac.com>
2018-09-07 22:04:34 +0200

    App: linting

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,16 +18,16 @@ class App extends Component {
       <BrowserRouter>
         <Layout>
           <Switch>
-            <Route exact path="/" component={Home}  />
+            <Route exact path="/" component={Home} />
 
-            <Route exact path="/posts" component={PostsAll}  />
-            <ProtectedRoute path="/posts/new" component={PostsNew}  />
-            <Route path="/posts/:id([0-9a-fA-F]{24})" component={PostsOne}  />
-            <Route path="/posts/:category(\D+)" component={PostsAll}  />
+            <Route exact path="/posts" component={PostsAll} />
+            <ProtectedRoute path="/posts/new" component={PostsNew} />
+            <Route path="/posts/:id([0-9a-fA-F]{24})" component={PostsOne} />
+            <Route path="/posts/:category(\D+)" component={PostsAll} />
 
-            <Route exact path="/users/new" component={UsersNew}  />
-            <Route exact path="/users/login" component={Login}  />
-            <Route exact path="/users/logout" component={Logout}  />
+            <Route exact path="/users/new" component={UsersNew} />
+            <Route exact path="/users/login" component={Login} />
+            <Route exact path="/users/logout" component={Logout} />
           </Switch>
         </Layout>
       </BrowserRouter>
