2316c7e825476d991179926fab0fc78050fab086
robertschaap <robert.schaap@mac.com>
2018-09-08 21:21:59 +0200

    comments controller: implemented basic logic

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
 router.post("/new", (req, res) => {
-  console.log(req.body);
-  res.json("done");
+  const comment = {
+    body: req.body.comment,
+    userId: req.body.userId,
+    postId: req.body.comment,
+  };
+
+  db.Comments.new(comment).then(comment => {
+    res.json(comment);
+  });
+
 });
 
 module.exports = router;
