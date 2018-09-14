0dab06b87d172d6d47f07d0156e1362ec1d2b01e
robertschaap <robert.schaap@mac.com>
2018-09-14 19:38:38 +0200

    added redux-promise middleware

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 import { Provider } from "react-redux";
-import { store } from "./redux.js";
+import { createStore, applyMiddleware } from "redux";
+import { reducers } from "./redux.js";
+import reduxPromise from "redux-promise";
 
 import "./styles/index.scss";
+
+const store = createStore(
+  reducers,
+  applyMiddleware(reduxPromise),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/redux.js
+++ src/redux.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { combineReducers } from "redux";
 
 const initialPostState = {
   post: {
@@ -83,12 +83,6 @@ export const setUser = (payload) => ({
   type: SET_USER,
   payload
 });
-
-// Store
-export const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 // Selector
 export const getLoadOnePost = state => state.onePost;
