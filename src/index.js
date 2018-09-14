43d9f59a31bfe4a542c8b6b5a5cfed856e208f2e
robertschaap <robert.schaap@mac.com>
2018-09-14 20:39:39 +0200

    index.js: fix on redux middleware

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import "./styles/index.scss";
 
 const store = createStore(
   reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(reduxPromise),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
