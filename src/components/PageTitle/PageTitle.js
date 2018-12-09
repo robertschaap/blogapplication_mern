b0df26c57d4b7940927369155fa66032f8674d1d
robertschaap <robert.schaap@mac.com>
2018-12-09 22:38:17 +0100

    PageTitle: converted functional to memo component

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const PageTitle = ({ title }) => {
+const PageTitle = React.memo(({ title }) => {
   const pageTitle = `Dotted Pixels - ${title}`;
-  
+
   return (
     <Helmet>
       <title>{pageTitle}</title>
     </Helmet>
   );
-};
+});
 
 export default PageTitle;
