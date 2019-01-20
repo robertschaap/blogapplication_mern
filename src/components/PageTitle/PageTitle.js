f9cc1ee0ea620a2ee0d17c49d9d6c8030a43cb0e
robertschaap <robert.schaap@mac.com>
2019-01-20 10:12:31 +0100

    PageTitle: added flow types

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,14 +1,23 @@
+// @flow
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const PageTitle = React.memo(({ title }) => {
-  const pageTitle = title ? `Dotted Pixels: ${title}` : "Dotted Pixels";
+type PageTitlePropsType = {
+  title?: string,
+}
 
-  return (
-    <Helmet>
-      <title>{pageTitle}</title>
-    </Helmet>
-  );
-});
+class PageTitle extends React.PureComponent<PageTitlePropsType>  {
+  render() {
+    const { title } = this.props;
+
+    const pageTitle = title ? `Dotted Pixels: ${title}` : "Dotted Pixels";
+
+    return (
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+    );
+  }
+}
 
 export default PageTitle;
--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,3 +1,4 @@
+// @flow
 import PageTitle from "./PageTitle";
 
 export default PageTitle;
