8d6bd8c20931c29b0e042b5260e6bf249446ae40
robertschaap <robert.schaap@mac.com>
2019-01-20 20:21:02 +0100

    ProtectedRouteContainer: added flow types

--- src/containers/ProtectedRouteContainer.js
+++ src/containers/ProtectedRouteContainer.js
@@ -1,10 +1,16 @@
+// @flow
 import React from "react";
 
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { getAuth } from "../ducks/auth";
 
-const ProtectedRouteContainer = ({ loggedIn, ...props }) => (
+type ProtectedRouteContainerPropsType = {
+  loggedIn: boolean,
+  props: *,
+};
+
+const ProtectedRouteContainer = ({ loggedIn, ...props }: ProtectedRouteContainerPropsType) => (
   loggedIn ? <Route {...props} /> : <Redirect to="/" />
 );
 
