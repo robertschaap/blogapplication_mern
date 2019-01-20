b71a03618585785e1fbc06de91ab3a6d05d731d1
robertschaap <robert.schaap@mac.com>
2019-01-20 19:52:26 +0100

    LoginMenu: added flow types

--- src/components/LoginMenu/LoginMenu.js
+++ src/components/LoginMenu/LoginMenu.js
@@ -1,3 +1,4 @@
+// @flow
 import React from "react";
 import styles from "./LoginMenu.scss";
 import { Link } from "react-router-dom";
--- src/components/LoginMenu/index.js
+++ src/components/LoginMenu/index.js
@@ -1,3 +1,4 @@
+// @flow
 import LoginMenu from "./LoginMenu";
 
 export default LoginMenu;
