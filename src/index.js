902168a0048b6642d13eb4674a0efca9c870c132
robertschaap <robert.schaap@mac.com>
2019-01-26 12:33:20 +0100

    package: installed redux-thunk

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { reducers } from "./redux.js";
 import reduxPromise from "redux-promise";
+import reduxThunk from "redux-thunk";
 
 import "./styles/normalize.css";
 import "./styles/index.scss";
@@ -14,7 +15,7 @@ import "./styles/index.scss";
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(reduxPromise),
+  applyMiddleware(reduxPromise, reduxThunk),
 );
 
 const root = document.getElementById("root");
